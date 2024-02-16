6f213a00a64e08ed9af8db1cdb623c9c21e75bb1
Filain <ratecurrent@gmail.com>
2024-02-16 17:02:20 +0200

    add ts, eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,18 @@
 module.exports = {
-    extends: [
-        "plugin:prettier/recommended",
-        "plugin:@typescript-eslint/recommended",
+  extends: ["plugin:prettier/recommended", "plugin:@typescript-eslint/recommended"],
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint", "simple-import-sort"],
+  root: true,
+  rules: {
+    "simple-import-sort/imports": "error",
+    "simple-import-sort/exports": "error",
+    "max-len": ["error", { code: 180 }],
+    "no-unused-vars": ["error", { argsIgnorePattern: "req|res|next" }],
+    "prettier/prettier": [
+      "error",
+      {
+        printWidth: 180,
+      },
     ],
-    parser: "@typescript-eslint/parser",
-    plugins: ["@typescript-eslint", "simple-import-sort"],
-    root: true,
-    rules: {
-        "simple-import-sort/imports": "error",
-        "simple-import-sort/exports": "error",
-        "max-len": ["error", { code: 180 }],
-        "no-unused-vars": ["error", { argsIgnorePattern: "req|res|next" }],
-        "prettier/prettier": [
-            "error",
-            {
-                printWidth: 180,
-            },
-        ],
-    },
+  },
 };
-
--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,9 @@
-const express = require('express')
+const express = require("express");
 
-const app = express()
+const app = express();
 
+const PORT = 3000;
 
-const PORT = 3000
-app.listen(PORT, ()=>{
-    console.log(`Server has start on PORT ${PORT}`)
-})
+app.listen(PORT, () => {
+  console.log(`Server has start on PORT ${PORT}`);
+});
